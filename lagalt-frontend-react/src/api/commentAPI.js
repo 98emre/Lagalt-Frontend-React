04bf17e9b08e57f199139fb98d07349a92b948b8
98emre <emre-98@live.se>
2023-10-05 15:18:38 +0200

    add comment to project

--- lagalt-frontend-react/src/api/commentAPI.js
+++ lagalt-frontend-react/src/api/commentAPI.js
@@ -5,12 +5,13 @@ import axios from 'axios';
 const BASE_URL = "http://localhost:8080/api/comments";
 
 
-export const addComment = createAsyncThunk("comment/addComment", async(comment, projectId, token) => {
+export const addComment = createAsyncThunk("comment/addComment", async({comment, projectId, token}) => {
     if (!token) {
         throw { message: "Token is not available" };
     }
    
     try {
+        console.log("innne")
         const response = await axios.post(`${BASE_URL}/project/${projectId}/add-comment`, comment,{
             headers:{
                 Authorization: `Bearer ${token}`,
@@ -18,7 +19,6 @@ export const addComment = createAsyncThunk("comment/addComment", async(comment, 
             }
         });
 
-        console.log("Comment ", response.data);
         return response.data;
 
     } catch (error) {
--- lagalt-frontend-react/src/components/project/ProjectDetail.js
+++ lagalt-frontend-react/src/components/project/ProjectDetail.js
@@ -5,10 +5,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getProjectById } from '../../api/projectAPI';
 import { getUserById } from '../../api/userAPI';
 
+import { useForm } from 'react-hook-form';
+import {addComment} from "../../api/commentAPI";
+import { useKeycloak } from '@react-keycloak/web';
+
 function ProjectDetail() {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const { register, handleSubmit, errors, reset } = useForm();
 
     const { id:stringId } = useParams();
     const id = parseInt(stringId, 10);
@@ -16,6 +21,8 @@ function ProjectDetail() {
     const project = useSelector((state) => state.project.project);
     const[user, setUser] = useState({});
        
+    const {keycloak} = useKeycloak();
+
     useEffect(() => {
         if (project.id != id || !user.username) {
             dispatch(getProjectById({id: id}))
@@ -35,6 +42,24 @@ function ProjectDetail() {
         return <p>Project not found.</p>;
     }
 
+    const onSubmit = (data) => {
+        console.log("prrr ", project)
+        const comment = {
+            id: null,
+            text: data.comment,
+            date: new Date(),
+            userId: user.id,
+            projectId: project.id
+        };
+
+        dispatch(
+            addComment({
+                comment: comment,
+                projectId: project.id,
+                token: keycloak.token
+        }))
+        reset();
+    };
 
   return (
     <div>
@@ -43,6 +68,12 @@ function ProjectDetail() {
         <p><strong>Description: </strong>{project.descriptions}</p>
         <p><strong>Category:</strong> {project.category}</p>
         <p><strong>Status:</strong> {project.status?.split("_").join(" ") || 'N/A'}</p>
+
+         <form onSubmit={handleSubmit(onSubmit)}>
+            <textarea {...register("comment", {minLength: 3})}></textarea>
+            {errors?.comment?.message && <p style={{color: 'red'}}>{errors.comment.message}</p>}
+            <input type="submit" value="Add Comment" />
+         </form>
 
         <button onClick={() => navigate(-1)}>Go Back</button>
     </div>
